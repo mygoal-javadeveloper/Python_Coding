11f57266099fd0047fa81bafb0ba9368a0ae72f2
Nisrin Dhoondia <38102371+mygoal-javadeveloper@users.noreply.github.com>
2020-03-19 14:44:24 +0530

    Update reverselist.py

--- Learning/reverselist.py
+++ Learning/reverselist.py
@@ -3,4 +3,6 @@ numbers=[6, 0, 2, 2, 1, 4, 1, 5]
 def reverselst(lst):
     return lst[-1:-(len(lst)+1):-1]
 
-print(reverselst(numbers))
+print(reverselst(numbers)) #doesnot reverse the original list
+numbers.reverse() #reverse the original list
+print(numbers)
